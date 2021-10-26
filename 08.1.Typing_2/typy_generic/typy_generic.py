f9be20f0cc04d3d5ba95269b96e066bd94457d9f
Auto Exporter <auto@yandex-team.ru>
2021-10-26 18:10:00 +0300

    Export public files

---
+++ 08.1.Typing_2/typy_generic/typy_generic.py
@@ -0,0 +1,13 @@
+import typing as tp
+
+
+class Pair:
+    def __init__(self, a, b): pass
+
+    def sum(self): pass
+
+    def first(self): pass
+
+    def second(self): pass
+
+    def __iadd__(self, pair): pass
