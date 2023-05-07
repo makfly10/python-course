4fb52f38ce3f62d2602941e00cd2f32b242bf651
makfly10 <makfly10@gmail.com>
2023-05-06 21:57:19 -0400

    Filter first sorted list by other list

--- 01.2.BasicTypes/filter_list_by_list/filter_list_by_list.py
+++ 01.2.BasicTypes/filter_list_by_list/filter_list_by_list.py
@@ -27,7 +27,7 @@ def filter_list_by_list_with_set(
     return lst_a_without_b
 
 
-def filter_list_by_list_with_set(
+def filter_list_by_list(
         lst_a: tp.Union[list[int], range], lst_b: tp.Union[list[int], range],
 ) -> list[int]:
     """
@@ -36,4 +36,29 @@ def filter_list_by_list_with_set(
     :param lst_b: second sorted list
     :return: filtered sorted list
     """
-    pass
+    if lst_a == [] or lst_b == []:
+        return lst_a
+    if lst_a == lst_b:
+        return []
+    a_list_goes_before_b = lst_b[0] > lst_a[-1]
+    a_list_goes_after_b = lst_a[0] > lst_b[-1]
+    if a_list_goes_before_b or a_list_goes_after_b:
+        return lst_a
+
+    lst_a_without_b = []
+    index_a = 0
+    index_b = 0
+    while index_a < len(lst_a) and index_b < len(lst_b):
+        list_b_value = lst_b[index_b]
+        list_a_value = lst_a[index_a]
+        if list_b_value > list_a_value:
+            lst_a_without_b.append(list_a_value)
+            index_a += 1
+        elif list_b_value < list_a_value:
+            index_b += 1
+        else:
+            index_a += 1
+    while index_a < len(lst_a):
+        lst_a_without_b.append(lst_a[index_a])
+        index_a += 1
+    return lst_a_without_b
