f94d94ef930083943f67afcb317d0e30cacc3116
makfly10 <makfly10@gmail.com>
2023-05-06 21:45:51 -0400

    Solve binary search

--- 01.2.BasicTypes/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/bin_basic/bin_basic.py
@@ -8,3 +8,19 @@ def find_value(nums: tp.Union[list[int], range], value: int) -> bool:
     :param value: integer to find
     :return: True if value exists, False otherwise
     """
+    if nums == [] or value > nums[-1] or value < nums[0]:
+        return False
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        middle = (low + high) // 2
+        if value > nums[middle]:
+            low = middle + 1
+            high = high + (len(nums) - high)
+        elif value < nums[middle]:
+            high = middle - 1
+        elif value == nums[middle] or value == nums[low] or value == nums[high]:
+            return True
+    if low > high:
+        return False
+    return True
