46ddfd5b1351ae6bab2e91567c2917a4291335ea
makfly10 <makfly10@gmail.com>
2023-05-08 16:16:42 -0400

    Solve fizz_buzz checking if the number is a multiple of 3, 5 or 15

--- 01.2.BasicTypes/fizz_buzz/fizz_buzz.py
+++ 01.2.BasicTypes/fizz_buzz/fizz_buzz.py
@@ -10,3 +10,15 @@ def get_fizz_buzz(n: int) -> list[tp.Union[int, str]]:
     :param n: size of sequence
     :return: list of values.
     """
+
+    answer = []
+    for figure in range(1, n + 1, 1):
+        if figure % 15 == 0:
+            answer.append('FizzBuzz')
+        elif figure % 3 == 0:
+            answer.append('Fizz')
+        elif figure % 5 == 0:
+            answer.append('Buzz')
+        else:
+            answer.append(figure)
+    return answer
