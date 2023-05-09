1565cb2d4bfd3724d4cf640d83cc6ece4787d19e
makfly10 <makfly10@gmail.com>
2023-05-09 16:28:59 -0400

    Take three values and return the middle value

--- 01.2.BasicTypes/middle_value_of_triple/middle_value_of_triple.py
+++ 01.2.BasicTypes/middle_value_of_triple/middle_value_of_triple.py
@@ -2,3 +2,10 @@ def get_middle_value(a: int, b: int, c: int) -> int:
     """
     Takes three values and returns middle value.
     """
+    list_ = []
+    list_.append(a)
+    list_.append(b)
+    list_.append(c)
+    list_sort = sorted(list_)
+    return list_sort[1]
+
