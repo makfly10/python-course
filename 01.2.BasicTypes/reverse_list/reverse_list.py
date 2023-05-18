3c815f96cb7ed2386888737895d245f53bb63a84
makfly10 <makfly10@gmail.com>
2023-05-18 15:30:35 -0400

    Return returned list using different techniques

--- 01.2.BasicTypes/reverse_list/reverse_list.py
+++ 01.2.BasicTypes/reverse_list/reverse_list.py
@@ -4,7 +4,12 @@ def reverse_iterative(lst: list[int]) -> list[int]:
     :param lst: input list
     :return: reversed list
     """
-
+    reversed_lst = []
+    lst_index = -1
+    while lst_index >= -len(lst):
+        reversed_lst.append(lst[lst_index])
+        lst_index -= 1
+    return reversed_lst
 
 def reverse_inplace_iterative(lst: list[int]) -> None:
     """
@@ -12,7 +17,16 @@ def reverse_inplace_iterative(lst: list[int]) -> None:
     :param lst: input list
     :return: None
     """
-
+    if lst == []:
+        return lst
+    if len(lst) == 1:
+        return lst
+    low = 0
+    high = -1
+    while low < (len(lst) //2):
+        lst[low], lst[high] = lst[high], lst[low]
+        low += 1
+        high -= 1
 
 def reverse_inplace(lst: list[int]) -> None:
     """
@@ -20,7 +34,7 @@ def reverse_inplace(lst: list[int]) -> None:
     :param lst: input list
     :return: None
     """
-
+    return lst.reverse()
 
 def reverse_reversed(lst: list[int]) -> list[int]:
     """
@@ -28,7 +42,7 @@ def reverse_reversed(lst: list[int]) -> list[int]:
     :param lst: input list
     :return: reversed list
     """
-
+    return list(reversed(lst))
 
 def reverse_slice(lst: list[int]) -> list[int]:
     """
@@ -36,3 +50,4 @@ def reverse_slice(lst: list[int]) -> list[int]:
     :param lst: input list
     :return: reversed list
     """
+    return lst[::-1]
