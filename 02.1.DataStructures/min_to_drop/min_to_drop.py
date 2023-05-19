43d540c4d5991de88902e99ef29915633e0b8478
makfly10 <makfly10@gmail.com>
2023-05-19 19:25:23 -0400

    Find min number of elements to drop so all the staying elements are equal

--- 02.1.DataStructures/min_to_drop/min_to_drop.py
+++ 02.1.DataStructures/min_to_drop/min_to_drop.py
@@ -1,8 +1,17 @@
 import typing as tp
-
+from collections import Counter
 
 def get_min_to_drop(seq: tp.Sequence[tp.Any]) -> int:
     """
     :param seq: sequence of elements
     :return: number of elements need to drop to leave equal elements
     """
+    if seq == [] or len(seq) == 1:
+        return 0
+    counter_seq = Counter(seq)
+    count_drop = 0
+    max_value = 0
+    for key, value in counter_seq.items():
+        if value > max_value:
+            max_value = value
+    return len(seq) - max_value
