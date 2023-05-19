39ebc99bb9552d922b188410d950ae5676cfc78e
makfly10 <makfly10@gmail.com>
2023-05-19 19:26:30 -0400

    Return reverted dictionary where keys become values and vice versa

--- 02.1.DataStructures/reverse_dict/reverse_dict.py
+++ 02.1.DataStructures/reverse_dict/reverse_dict.py
@@ -6,3 +6,9 @@ def revert(dct: tp.Mapping[str, str]) -> dict[str, list[str]]:
     :param dct: dictionary to revert in format {key: value}
     :return: reverted dictionary {value: [key1, key2, key3]}
     """
+    dict_reversed = {}
+    for key, value in dct.items():
+        if value not in dict_reversed:
+            dict_reversed[value] = []
+        dict_reversed[value].append(key)
+    return dict_reversed
