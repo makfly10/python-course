be71a4c255e9a85f4f8798d74f3c7a85f5fbdb43
makfly10 <makfly10@gmail.com>
2023-05-24 17:04:30 -0400

    Encrypt message using caesar cipher

--- 03.1.FunctionsStringsIO/caesar_cipher/caesar_cipher.py
+++ 03.1.FunctionsStringsIO/caesar_cipher/caesar_cipher.py
@@ -5,3 +5,18 @@ def caesar_encrypt(message: str, n: int) -> str:
     :param n: shift
     :return: encrypted message
     """
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    lookup = {letter: i for i, letter in enumerate(alphabet)}
+
+    encrypted = ''
+    for char in message:
+        if char.isalpha():
+            is_upper = char.isupper()
+            char_index = lookup[char.lower()]
+            shifted_index = (char_index + n) % 26
+            shifted_char = alphabet[shifted_index]
+            encrypted += shifted_char.upper() if is_upper == True else shifted_char
+        else:
+            encrypted += char
+
+    return encrypted
