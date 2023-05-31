cef36f9ea16c848f0486b1b49d009e681b717fd9
makfly10 <makfly10@gmail.com>
2023-05-31 13:11:40 -0400

    Return max element before zero for input array.

--- 03.2.NumpyAndPandas/max_element/max_element.py
+++ 03.2.NumpyAndPandas/max_element/max_element.py
@@ -10,3 +10,10 @@ def max_element(array: np.ndarray) -> tp.Optional[float]:
     :param array: array,
     :return: max element value or None
     """
+    if 0 not in array:
+        return None
+    zero_indices = np.where(array[:-1] == 0)[0]
+    if len(zero_indices) == 0:
+        return None
+    return np.max(array[zero_indices+1])
+
